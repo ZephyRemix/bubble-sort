3c70da06254e88d4d62de94d90587e73c80bba70
ZephyRemix <hunyaochong@gmail.com>
2024-07-11 15:54:21 +0800

    Insert comments to improve readability

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,32 +1,26 @@
-
 def bubble_sort(num_arr)
 
   loop do
-    # reset flag to false
+    # records swap footprint in each iteration
+    # reset flag element to false after each loop
     elements_swapped = false
 
     num_arr.each_with_index do |val, i|
-      # if first value bigger than second value, swap values
+      # skip the final element
       next if i == num_arr.length - 1
-      case num_arr[i] <=> num_arr[i+1] 
-      when 1
-        num_arr[i] = num_arr[i+1]
-        num_arr[i+1] = val 
+
+      # if first value bigger than second value, swap values
+      if (num_arr[i] <=> num_arr[i+1]) == 1
+        num_arr[i] = num_arr[i+1]; num_arr[i+1] = val 
         elements_swapped = true
-      end
-    end 
-    # binding.pry
+      end 
+
+    end
+    # end loop; no elements indicates sorted array
     break unless elements_swapped
   end
-  
   num_arr
 end
 
 
-def swap_elements(x, y)
-  x,y= y,x
-end
-
-
 sorted_arr = bubble_sort([4,3,78,2,0,2])
-p sorted_arr
